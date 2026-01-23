73e0897bac8894282eed0f3ff8a6306f39260c00
Bryan2397 <bryan.mirtil@gmail.com>
2026-01-23 17:20:03 -0500

    Created signup page and added no implicit any types for better readability and safety

--- internship-tracker/src/pages/Signup.tsx
+++ internship-tracker/src/pages/Signup.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import axios from "axios";
+
 const backgroundStyle: React.CSSProperties = {
   minHeight: "100vh",
   background: "linear-gradient(135deg, #0d6efd, #6ea8fe)",
@@ -7,6 +10,32 @@ const backgroundStyle: React.CSSProperties = {
 };
 
 const Signup = () => {
+  interface SignupForm {
+    email: string;
+    password: string;
+  }
+
+  const handleChange = (update: Partial<SignupForm>) => {
+    setSignup((signup) => ({ ...signup, ...update }));
+    setTimeout(() => {
+      console.log(signup);
+    }, 1000);
+  };
+
+  async function handleSubmit() {
+    try {
+      const res = axios.post("http://localhost:8080/api/auth/signup", signup);
+      console.log(res);
+    } catch (error) {
+      console.log("error message: ", error);
+    }
+  }
+
+  const [signup, setSignup] = useState<SignupForm>({
+    email: "",
+    password: "",
+  });
+
   return (
     <div style={backgroundStyle}>
       <div className="container" style={{ maxWidth: "450px" }}>
@@ -20,10 +49,25 @@ const Signup = () => {
                 type="email"
                 className="form-control"
                 placeholder="Enter email"
+                name="email"
+                value={signup.email}
+                onChange={(e) => handleChange({ email: e.target.value })}
+              />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Password</label>
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Password"
+                name="password"
+                value={signup.password}
+                onChange={(e) => handleChange({ password: e.target.value })}
               />
             </div>
           </form>
           <button
+            onClick={handleSubmit}
             style={{
               borderRadius: "5px",
               height: "40px",
--- internship-tracker/src/tester.tsx
+++
@@ -1,5 +0,0 @@
-function add(y: number): number {
-  return y + 5;
-}
-
-console.log(add(10));
