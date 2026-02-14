a87aa0eee52c791b7434ea507cd693ed606dae63
Bryan2397 <bryan.mirtil@gmail.com>
2026-02-14 17:53:29 -0500

    enhanced UX for job application status

--- internship-tracker/src/pages/JobRow.tsx
+++ internship-tracker/src/pages/JobRow.tsx
@@ -1,11 +1,30 @@
 import { Job } from "../types/Job";
 
 export const JobRow = (job: Job) => {
+  const getStatusClass = (status: string) => {
+    switch (status) {
+      case "APPLIED":
+        return "badge bg-primary";
+      case "OA":
+        return "badge bg-dark";
+      case "INTERVIEW":
+        return "badge bg-info";
+      case "OFFER":
+        return "badge bg-success";
+      case "REJECTED":
+        return "badge bg-danger";
+    }
+    return "";
+  };
+
   return (
     <tr>
-      <td>{job.url}</td>
+      <td className={getStatusClass(job.status)} style={{ color: "white" }}>
+        {job.status}
+      </td>
       <td>{job.company}</td>
       <td>{job.addedOn}</td>
+      <td>{job.url}</td>
     </tr>
   );
 };
