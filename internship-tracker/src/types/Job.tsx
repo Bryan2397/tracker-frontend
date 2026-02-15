565cc1a75986a768fde54677680c7fb7b0ab3eaa
Bryan2397 <bryan.mirtil@gmail.com>
2026-02-15 16:46:30 -0500

    added new fields for more specificity

--- internship-tracker/src/types/Job.tsx
+++ internship-tracker/src/types/Job.tsx
@@ -1,5 +1,11 @@
 export interface Job {
+  title: string;
   url: string;
+  dateApplied?: string;
   company: string;
   addedOn: string;
+  status: "APPLIED" | "OA" | "INTERVIEW" | "OFFER" | "REJECTED";
+  notes?: string;
+  jobSummary: string;
+  location?: string;
 }
